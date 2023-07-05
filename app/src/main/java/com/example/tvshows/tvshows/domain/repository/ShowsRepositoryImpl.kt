3cdccc097751a47fa079938bba6697c6a6309aad
dovdtel87 <dmgdavid2109@gmail.com>
2023-07-05 17:47:33 +0200

    Implements repository pattern with cached data

--- app/src/main/java/com/example/tvshows/tvshows/domain/repository/ShowsRepositoryImpl.kt
+++ app/src/main/java/com/example/tvshows/tvshows/domain/repository/ShowsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.tvshows.tvshows.domain.repository
 
+import com.example.tvshows.data.database.ShowDao
 import com.example.tvshows.data.mapper.ShowsMapper
 import com.example.tvshows.data.model.Show
 import com.example.tvshows.data.network.api.ShowsApi
@@ -8,13 +9,20 @@ import javax.inject.Inject
 
 class ShowsRepositoryImpl @Inject constructor(
     private val showsApi: ShowsApi,
-    private val mapper : ShowsMapper,
+    private val mapper: ShowsMapper,
+    private val showDao: ShowDao
 ) : ShowsRepository {
     override suspend fun fetchShows(): Result<List<Show>> {
         return try {
-            val apiShows = showsApi.fetchShows().list
-            val mappedShows = mapper.map(apiShows)
-            Result.success(mappedShows)
+            val cachedShows = mapper.mapToShowFromEntity(showDao.getShows())
+            if (cachedShows.isNotEmpty()) {
+                Result.success(cachedShows)
+            } else {
+                val apiShows = showsApi.fetchShows().list
+                showDao.insertShows(mapper.mapToShowEntity(apiShows))
+                val mappedShows = mapper.map(apiShows)
+                Result.success(mappedShows)
+            }
         } catch (exception: Exception) {
             Result.failure(exception)
         }
