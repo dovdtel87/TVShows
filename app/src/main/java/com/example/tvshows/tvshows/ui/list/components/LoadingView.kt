6b8a566b9e3e8ca911703692e1954e4f8947b95a
dovdtel87 <dmgdavid2109@gmail.com>
2023-07-05 13:02:12 +0200

    Adds LoadingView

---
+++ app/src/main/java/com/example/tvshows/tvshows/ui/list/components/LoadingView.kt
@@ -0,0 +1,27 @@
+package com.example.tvshows.tvshows.ui.list.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoadingView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingScreenPreview() {
+    LoadingView()
+}
