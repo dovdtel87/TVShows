a4a4155f002ecc0864ac59db487d4c996da2e395
dovdtel87 <dmgdavid2109@gmail.com>
2023-07-05 17:46:57 +0200

    Adds mappers to and from the database

--- app/src/main/java/com/example/tvshows/data/mapper/ShowsMapper.kt
+++ app/src/main/java/com/example/tvshows/data/mapper/ShowsMapper.kt
@@ -1,5 +1,6 @@
 package com.example.tvshows.data.mapper
 
+import com.example.tvshows.data.database.ShowEntity
 import com.example.tvshows.data.model.Show
 import com.example.tvshows.data.network.model.ShowDto
 import javax.inject.Inject
@@ -25,4 +26,32 @@ class ShowsMapper @Inject constructor() {
     }
 
     private fun String.generateUrl(): String = IMAGE_URL_PREFIX + this
+
+    fun mapToShowEntity(showsDto: List<ShowDto>) : List<ShowEntity> {
+        val showListEntity = mutableListOf<ShowEntity>()
+        showsDto.forEach{
+            showListEntity.add(
+                ShowEntity(
+                    id =  it.id,
+                    name = it.name,
+                    path = it.path
+                )
+            )
+        }
+        return showListEntity
+    }
+
+    fun mapToShowFromEntity(showsDto: List<ShowEntity>) : List<Show> {
+        val showList = mutableListOf<Show>()
+        showsDto.forEach{
+            showList.add(
+                Show(
+                    id =  it.id,
+                    name = it.name,
+                    url = it.path.generateUrl()
+                )
+            )
+        }
+        return showList
+    }
 }
