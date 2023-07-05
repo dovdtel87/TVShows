6752b98cd970ef91bcd14ae4101e54f1a395022e
dovdtel87 <dmgdavid2109@gmail.com>
2023-07-05 13:23:15 +0200

    Creates ListShowsScreen

--- app/src/main/java/com/example/tvshows/tvshows/ui/list/screen/ListShowsScreen.kt
+++ app/src/main/java/com/example/tvshows/tvshows/ui/list/screen/ListShowsScreen.kt
@@ -1,4 +1,35 @@
 package com.example.tvshows.tvshows.ui.list.screen
 
-class ListShowsScreen {
+import androidx.compose.runtime.Composable
+import com.example.tvshows.tvshows.ui.ListShowsViewModel
+
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.tvshows.tvshows.ui.list.components.ErrorView
+import com.example.tvshows.tvshows.ui.list.components.ListShows
+import com.example.tvshows.tvshows.ui.list.components.LoadingView
+import com.example.tvshows.tvshows.ui.state.ListScreenState
+
+@Composable
+fun ListShowsScreen(
+    viewModel: ListShowsViewModel = hiltViewModel(),
+) {
+    when (val uiState = viewModel.state.collectAsStateWithLifecycle().value) {
+        is ListScreenState.Loading -> {
+            LoadingView()
+        }
+
+        is ListScreenState.Error -> {
+            ErrorView(uiState.errorMessage) {
+                viewModel.fetchShows()
+            }
+        }
+
+        is ListScreenState.Content -> {
+            ListShows(
+                shows = uiState.shows,
+                onShortList = {}
+            )
+        }
+    }
 }
