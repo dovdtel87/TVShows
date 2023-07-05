4d6bff80a69dbe6466e592cbd29fc1a68d65215b
dovdtel87 <dmgdavid2109@gmail.com>
2023-07-05 18:27:11 +0200

    Creates a FakeRepository

---
+++ app/src/androidTest/java/com/example/tvshows/repository/FakeShowsRepository.kt
@@ -0,0 +1,13 @@
+package com.example.tvshows.repository
+
+import com.example.tvshows.data.model.Show
+import com.example.tvshows.data.repository.ShowsRepository
+
+class FakeShowsRepository : ShowsRepository {
+    override suspend fun fetchShows(): Result<List<Show>> {
+        val show1 = Show("1","Batman", "aUrl1")
+        val show2 = Show("2","Superman", "aUrl2")
+        val show3 = Show("3","Friends", "aUrl3")
+        return Result.success(listOf(show1, show2, show3))
+    }
+}
