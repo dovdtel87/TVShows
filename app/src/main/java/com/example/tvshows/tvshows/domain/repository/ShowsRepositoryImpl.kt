be3e7c5e07fc755ad0621de4b187a594ac580554
dovdtel87 <dmgdavid2109@gmail.com>
2023-07-05 12:36:26 +0200

    Implements Repository

--- app/src/main/java/com/example/tvshows/tvshows/domain/repository/ShowsRepositoryImpl.kt
+++ app/src/main/java/com/example/tvshows/tvshows/domain/repository/ShowsRepositoryImpl.kt
@@ -1,10 +1,22 @@
 package com.example.tvshows.tvshows.domain.repository
 
+import com.example.tvshows.data.mapper.ShowsMapper
 import com.example.tvshows.data.model.Show
+import com.example.tvshows.data.network.api.ShowsApi
 import com.example.tvshows.data.repository.ShowsRepository
+import javax.inject.Inject
 
-class ShowsRepositoryImpl : ShowsRepository {
+class ShowsRepositoryImpl @Inject constructor(
+    private val showsApi: ShowsApi,
+    private val mapper : ShowsMapper,
+) : ShowsRepository {
     override suspend fun fetchShows(): Result<List<Show>> {
-        TODO("Not yet implemented")
+        return try {
+            val apiShows = showsApi.fetchShows().list
+            val mappedShows = mapper.map(apiShows)
+            Result.success(mappedShows)
+        } catch (exception: Exception) {
+            Result.failure(exception)
+        }
     }
 }
